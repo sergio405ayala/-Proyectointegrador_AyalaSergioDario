81062a0e9fd5579757761d223f3aa199cf4e9056
sergio405ayala <99102058+sergio405ayala@users.noreply.github.com>
2022-06-13 13:03:39 -0300

    cir

--- Proyectointegrador_AyalaSergioDario/src/app/app.module.ts
+++ Proyectointegrador_AyalaSergioDario/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -20,10 +22,12 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
+    NgCircleProgressModule.forRoot({})
     AppRoutingModule
   ],
   providers: [],
