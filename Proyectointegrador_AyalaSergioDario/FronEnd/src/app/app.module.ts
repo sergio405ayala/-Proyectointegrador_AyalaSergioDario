9ef4f420471014ff51ea6407f5fe96d2c66a62e3
sergio405ayala <99102058+sergio405ayala@users.noreply.github.com>
2022-06-18 19:17:16 -0300

    login

--- Proyectointegrador_AyalaSergioDario/FronEnd/src/app/app.module.ts
+++ Proyectointegrador_AyalaSergioDario/FronEnd/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClient, HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import {HttpClient, HttpClientModule } from "@angular/common/http";
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
