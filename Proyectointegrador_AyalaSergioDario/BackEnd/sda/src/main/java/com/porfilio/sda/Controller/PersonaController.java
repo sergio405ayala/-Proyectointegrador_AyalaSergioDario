42459bc4304040c40ae485b4b5492c1a6b806e0d
sergio405ayala <99102058+sergio405ayala@users.noreply.github.com>
2022-06-18 16:49:51 -0300

    actualizacion

--- Proyectointegrador_AyalaSergioDario/BackEnd/sda/src/main/java/com/porfilio/sda/Controller/PersonaController.java
+++ Proyectointegrador_AyalaSergioDario/BackEnd/sda/src/main/java/com/porfilio/sda/Controller/PersonaController.java
@@ -5,8 +5,17 @@ import com.porfilio.sda.Entity.Persona;
 import com.porfilio.sda.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- Proyectointegrador_AyalaSergioDario/BackEnd/sda/src/main/java/com/porfilio/sda/Entity/Persona.java
+++ Proyectointegrador_AyalaSergioDario/BackEnd/sda/src/main/java/com/porfilio/sda/Entity/Persona.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 
